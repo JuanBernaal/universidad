5c1f42480150576e598e9a3bca3f27ce49d49663
JuanBernaal <projectjuandavid@gmail.com>
2023-11-01 21:49:05 -0500

    MVC design added and some changes to streamlit sim

--- 3 semestre/poo/proyectopy/pruebaStreamlit/prueba.py
+++ 3 semestre/poo/proyectopy/pruebaStreamlit/prueba.py
@@ -2,3 +2,21 @@ import streamlit as st
 
 st.title("This is an example")
 inpt = st.text_input("Introduce algo: ")
+st.text("Hello World")
+activated = st.toggle("Activate")
+st.link_button("DO NOT PRESS ME", "https://www.youtube.com/watch?v=xvFZjo5PgG0", type="primary") 
+
+st.sidebar.write("This lives in the sidebar")
+if (st.sidebar.button("Click me!")):
+    st.text("Si")
+
+st.latex("e^x * y^2")
+
+data = [
+    {"Elemento" : "Lo que quiera", "Asi si": 3},
+    {"Elemento" : "Lo que quiera", "Asi si" : 3}
+]
+
+st.table(data)
+
+st.warning("Unable to fetch image. Skipping...", icon="⚠️")
