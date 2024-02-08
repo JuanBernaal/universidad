321dc62a0310f9106a0605e6d88ab72de468eede
Juan David Bernal <projectjuandavid@gmail.com>
2024-02-07 22:17:37 -0500

    Added 2, 3 and 4

--- Semestre IV/Logica/taller1.v
+++ Semestre IV/Logica/taller1.v
@@ -1,3 +1,4 @@
+(*########################## Punto 1 ##########################*)
 Definition nor (a b : bool) : bool :=
     match a, b with 
     | false, false => true
@@ -17,3 +18,44 @@ Proof. simpl. reflexivity. Qed.
 
 Example testNor4 : nor true true = false.
 Proof. simpl. reflexivity. Qed. 
+
+(*########################## Punto 2 ##########################*)
+
+Definition neg (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition nor2 (a  b : bool) : bool :=
+    neg (orb a b).
+
+Example testNor2.1 : nor false false = true.
+Proof. simpl. reflexivity. Qed.
+
+(*########################## Punto 3 ##########################*)
+Module tiempo.
+Inductive tiempo : Type := 
+    | mañana
+    | mediodía
+    | tarde
+    | noche
+    | madrugada.
+
+(*########################## Punto 4 ##########################*)
+
+Definition nextTime (t : tiempo) : tiempo :=
+    match t with 
+    | madrugada => mañana
+    | mañana => mediodía
+    | mediodía => tarde
+    | tarde => noche
+    | noche => madrugada
+    end. 
+
+Example testNextTime1 : nextTime madrugada = mañana.
+Proof. simpl. reflexivity. Qed.
+
+(*########################## Punto 5 ##########################*)
+
+(**)
