679dd8f34ba96cd16207f421ff39764eda92f1ea
Juan David Bernal <projectjuandavid@gmail.com>
2024-02-07 21:10:46 -0500

    Taller 1, punto 1 coq Updated

--- Semestre IV/Logica/taller1.v
+++ Semestre IV/Logica/taller1.v
@@ -1,8 +1,19 @@
 Definition nor (a b : bool) : bool :=
     match a, b with 
     | false, false => true
-    | -, - => false
+    | false, true => false
+    | true, true => false
+    | true, false => false
     end.
 
 Example testNor1 : nor false false = true.
 Proof. simpl. reflexivity. Qed.
+
+Example testNor2 : nor true false = false.
+Proof. simpl. reflexivity. Qed. 
+
+Example testNor3 : nor false true = false.
+Proof. simpl. reflexivity. Qed.
+
+Example testNor4 : nor true true = false.
+Proof. simpl. reflexivity. Qed. 
